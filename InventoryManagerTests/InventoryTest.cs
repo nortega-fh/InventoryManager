baae5a5d142fe5bd2d803eb5dfdec1125eda9cb0
Nelson Ortega <nelson.ortega@foothillsolutions.com>
2024-01-19 01:38:57 -0500

    test: defined tests for editing a product

--- InventoryManagerTests/InventoryTest.cs
+++ InventoryManagerTests/InventoryTest.cs
@@ -4,8 +4,13 @@ using InventoryManager.Products;
 
 namespace InventoryManagerTests
 {
-    public class InventoryTest
+    public class InventoryTest: IDisposable
     {
+        public void Dispose()
+        {
+            Inventory.GetProducts().Clear();
+        }
+
         [Fact]
         public void TestAddProduct() {
             Product testProduct = new("test1", 25.1f, 2);
@@ -15,6 +20,29 @@ namespace InventoryManagerTests
             Assert.NotNull(Inventory.GetProducts());
             Assert.True(Inventory.GetProducts().Count != 0);
             Assert.Equal(Inventory.GetProducts().ElementAt(0), testProduct);
+        }
+
+        [Fact]
+        public void TestEditExistingProduct()
+        {
+            Product testProduct = new("test1", 25.1f, 2);
+            Inventory.AddProduct(testProduct);
+
+            Product modifiedTestProduct = new("test1", 35.5f, 10);
+            Inventory.EditProduct(modifiedTestProduct);
+
+            Product? resultOfEdit = Inventory.GetProducts().Find(p => p.Name.Equals("test1", StringComparison.Ordinal));
+
+            Assert.NotNull(resultOfEdit);
+            Assert.Equivalent(modifiedTestProduct, resultOfEdit);
+        }
+
+        [Fact]
+        public void TestEditNonExistingProduct()
+        {
+            Product testProduct = new("modifyNotExistingTest2", 25.1f, 2);
+
+            Assert.Throws<NoProductFoundException>(() => Inventory.EditProduct(testProduct));
         }
 
         [Fact]
