f568b42d0a877e1e9fed81181835342cb36bde7e
Nelson Ortega <nelson.ortega@foothillsolutions.com>
2024-01-19 01:39:12 -0500

    feat: implemented edit product functionality

--- InventoryManager/Inventory/Inventory.cs
+++ InventoryManager/Inventory/Inventory.cs
@@ -13,9 +13,22 @@ namespace InventoryManager.Inventory
         private static List<Product> products = [];
 
         public static List<Product> GetProducts() => products;
-        
+
         public static void AddProduct(Product product) => products.Add(product);
 
+        public static void EditProduct(Product product)
+        {
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Name == product.Name)
+                {
+                    products[i] = product;
+                    return;
+                }
+            }
+            throw new NoProductFoundException(product.Name);
+        }
+   
         public static List<Product> SearchProduct(string name)
         {
             List<Product> searchedProducts = products.FindAll(product => product.Name.Contains(name, StringComparison.OrdinalIgnoreCase));
