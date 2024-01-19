b53468a9a22922309f6d81305c1e11799678fe81
Nelson Ortega <nelson.ortega@foothillsolutions.com>
2024-01-19 00:32:44 -0500

    test: implemented Search Product functionality tests

--- InventoryManagerTests/InventoryTest.cs
+++ InventoryManagerTests/InventoryTest.cs
@@ -1,4 +1,5 @@
-﻿using InventoryManager.Inventory;
+﻿using InventoryManager.Exceptions;
+using InventoryManager.Inventory;
 using InventoryManager.Products;
 
 namespace InventoryManagerTests
@@ -14,6 +15,26 @@ namespace InventoryManagerTests
             Assert.NotNull(Inventory.GetProducts());
             Assert.True(Inventory.GetProducts().Count != 0);
             Assert.Equal(Inventory.GetProducts().ElementAt(0), testProduct);
+        }
+
+        [Fact]
+        public void TestSearchExistingProduct()
+        {
+            Product testProduct = new("test1", 25.1f, 2);
+
+            Inventory.AddProduct(testProduct);
+
+            List<Product> searchResult = Inventory.SearchProduct("test");
+
+            Assert.NotNull(searchResult);
+            Assert.Contains(testProduct, searchResult);
+            Assert.Equivalent(searchResult.ElementAt(0), testProduct);
+        }
+
+        [Fact]
+        public void TestSearchNonExistingProduct()
+        { 
+            Assert.Throws<NoProductFoundException>(() => Inventory.SearchProduct("test not existing product"));
         }
     }
 }
