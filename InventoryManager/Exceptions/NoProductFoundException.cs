72a9006141b48282e0d9ff373bb227d38a319bd3
Nelson Ortega <nelson.ortega@foothillsolutions.com>
2024-01-19 00:33:10 -0500

    feat: created NoProductFoundException type to send a message when a product is not found

---
+++ InventoryManager/Exceptions/NoProductFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManager.Exceptions
+{
+    public class NoProductFoundException(string name) : Exception($"There is no product with the following name: {name}")
+    {
+    }
+}
