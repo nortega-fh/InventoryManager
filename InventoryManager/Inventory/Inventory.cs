afce03fcfd04823202b80a4ed06cee8e30a599c3
Nelson Ortega <nelson.ortega@foothillsolutions.com>
2024-01-18 23:14:17 -0500

    feat: implemented logic in Inventory class for the addition of a product and obtantion of the product list

--- InventoryManager/Inventory/Inventory.cs
+++ InventoryManager/Inventory/Inventory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InventoryManager.Products;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace InventoryManager.Inventory
 {
-    internal class Inventory
+    public class Inventory
     {
+        private static List<Product> products = [];
+
+        public static List<Product> GetProducts() => products;
+        
+        public static void AddProduct(Product product) => products.Add(product);
+        
     }
 }
