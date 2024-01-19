55e0fe6591b757f4ab95771f759ab66bd724dd57
Nelson Ortega <nelson.ortega@foothillsolutions.com>
2024-01-18 23:12:36 -0500

    test: created test for adding a product into the inventory list

--- InventoryManagerTests/InventoryTest.cs
+++ InventoryManagerTests/InventoryTest.cs
@@ -1,11 +1,19 @@
-﻿namespace InventoryManagerTests
+﻿using InventoryManager.Inventory;
+using InventoryManager.Products;
+
+namespace InventoryManagerTests
 {
     public class InventoryTest
     {
         [Fact]
-        public void Test1()
-        {
+        public void TestAddProduct() {
+            Product testProduct = new("test1", 25.1f, 2);
 
+            Inventory.AddProduct(testProduct);
+
+            Assert.NotNull(Inventory.GetProducts());
+            Assert.True(Inventory.GetProducts().Count != 0);
+            Assert.Equal(Inventory.GetProducts().ElementAt(0), testProduct);
         }
     }
 }
